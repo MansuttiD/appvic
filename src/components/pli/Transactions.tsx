2b01a1fe211ee7622964aba5d1e156859c218496
MansuttiSereno <daniel.mansutti@serenoapp.com>
2023-09-20 20:33:54 -0400

    style: transactions stryles

--- src/components/pli/Transactions.tsx
+++ src/components/pli/Transactions.tsx
@@ -13,7 +13,7 @@ export default function Transactions() {
         <section className="flex flex-col bg-[#FFF] rounded-3xl justify-between p-6 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] md:flex-col md:items-start">
             <div className='flex justify-between'>
             <h3 className="text-base font-semibold">Mercantil</h3>
-            <HiOutlineClock className="text-[#1277DD] text-[18px]" />
+            <HiOutlineClock className="text-[#1277DD] text-[24px]" />
             </div>
             <h4><span className="text-[#00BA07]">Buy</span> $9446,3</h4>
             <h4>VOTCs Available, Ocucupied</h4>
@@ -22,7 +22,7 @@ export default function Transactions() {
         <section className="flex flex-col bg-[#FFF] rounded-3xl justify-between p-6 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] md:flex-col md:items-start">
             <div className='flex justify-between'>
             <h3 className="text-base font-semibold">Mercantil</h3>
-            <HiOutlineClock className="text-[#1277DD] text-[18px]" />
+            <HiOutlineClock className="text-[#1277DD] text-[24px]" />
             </div>
             <h4><span className="text-[#BA0000]">Sell</span> $9446,3</h4>
             <h4>VOTCs Available, Ocucupied</h4>
