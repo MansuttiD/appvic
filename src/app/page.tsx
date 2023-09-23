05bc0f72d6c313c838d63f5bdd44dedd45e54a8a
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2023-09-22 18:40:38 -0600

    feat: merge apiconnect and compnent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import Operations from "@/components/home/Operations";
 import RecentAtivity from "@/components/home/RecentActivity";
 import Welcome from "@/components/home/Welcome";
-import ExecutionPanel from "@/components/pli/ExecutionPanel";
 
 export default function Home() {
   return (
     <>
     <div className="bg-[#F8F8F6] min-h-[calc(100vh-70px)] md:min-h-[calc(100vh-100px)]">
-      <div className="max-w-[370px] hidden mx-auto sm:max-w-lg md:max-w-4xl lg:max-w-6xl xl:max-w-7xl pt-10">
+      <div className="max-w-[370px]  mx-auto sm:max-w-lg md:max-w-4xl lg:max-w-6xl xl:max-w-7xl pt-10">
         <Welcome/>
         <Operations/>
         <RecentAtivity/>
       </div>
-      <ExecutionPanel/>
     </div>
     </>
   );
