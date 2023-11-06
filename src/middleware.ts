74ff9194f13644d3401e1f8080d0a0765a2ccec2
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2023-11-06 15:40:09 -0600

    feat: change pages locked by token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    let jwt = request.cookies.get('tokenSereno');
-    if (request.nextUrl.pathname.startsWith('/login')||request.nextUrl.pathname.startsWith('/register') ) {
-      if (jwt) {
-        return NextResponse.redirect(new URL('/', request.url));
-      }
-    }else{
-      if (jwt == undefined) {
-        return NextResponse.redirect(new URL('/login', request.url));
-      }
-    }
+  let jwt = request.cookies.get('tokenSereno');
+
+  if (jwt == undefined ) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  export const config = {
-  matcher: '/(.*)?',
+}
+
+export const config = {
+  matcher: ['/','/pli','/pli(/.*)?','/history','/recharge','/send','/settings','/settings(/.*)?','/withdraw'],
 };
